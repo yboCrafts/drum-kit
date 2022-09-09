043e1ecb1ac8568d9378b661968728d775ca0f73
yboCrafts <ytimola123@gmail.com>
2022-09-09 15:30:12 +0800

    Add function play sound when click

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function playSound(e) {
+    // debugger
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; // stop the function from running all together
     audio.currentTime = 0; // rewind to the start
     audio.play()
     key.classList.add('playing');
-    console.log(e)
 }
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return; // skip it if its not a transform
@@ -16,3 +16,12 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('click', (e) => { // play when touch or click
+    const keyCode = e.target.closest('.key').dataset.key;
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+    if(!audio) return; // stop the function from running all together
+    audio.currentTime = 0; // rewind to the start
+    audio.play()
+    key.classList.add('playing');
+}));
